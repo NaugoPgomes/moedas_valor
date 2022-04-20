f8d303f258ca669eec6dba6aa6dc0dccc4c28814
benpg pcs <naugopgomes@gmail.com>
2022-04-20 20:18:01 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let valor = document.getElementById("valor_bitcoin")
+let valor_euro = document.getElementById("valor_euro")
+let valor_dollar = document.getElementById("valor_dollar")
+
+function pegar_valor_bitcoin()
+{
+    fetch("https://economia.awesomeapi.com.br/last/BTC-BRL")
+    .then(response =>
+    {
+        return response.json()
+    })
+    .then(data =>
+    {
+        var resultado = data.BTCBRL.bid
+        valor.innerHTML = `R$ : ${resultado}`
+        console.log(resultado)
+    })
+}
+
+function pegar_valor_euro()
+{
+    fetch("https://economia.awesomeapi.com.br/last/EUR-BRL")
+    .then(response =>
+    {
+        return response.json()
+    })
+    .then(data =>
+    {
+        var resultado = data.EURBRL.bid
+        valor_euro.innerHTML = `R$ : ${resultado}`
+        console.log(resultado)
+    })
+}
+
+function pegar_valor_dollar()
+{
+    fetch("https://economia.awesomeapi.com.br/last/USD-BRL")
+    .then(response =>
+    {
+        return response.json()
+    })
+    .then(data =>
+    {
+        var resultado = data.USDBRL.bid
+        valor_dollar.innerHTML = `R$ : ${resultado}`
+        console.log(resultado)
+    })
+}
+
+pegar_valor_bitcoin()
+pegar_valor_euro()
+pegar_valor_dollar()
+
+setInterval(() => 
+{
+    pegar_valor_bitcoin()
+    pegar_valor_euro()
+    pegar_valor_dollar()
+},30000)
